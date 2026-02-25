6b3afc896b5c4a5393aab144dae4476e2ed22c91
Jona-G <pribek.jonatan@gmail.com>
2026-02-25 13:59:42 +0100

    Remove welcome module, refactoring

---
+++ project/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Photo(models.Model):
+    name = models.CharField(max_length=40, unique=True, verbose_name="Name")
+    image = models.ImageField(upload_to='photos/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from welcome import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
