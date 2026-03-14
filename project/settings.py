dbbe2be5a971b90b1c6f67d9cfa000acc1db59aa
Jona-G <pribek.jonatan@gmail.com>
2026-03-14 21:07:55 +0100

    Update settings

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://felhoalapu-lab3-jonatanpribek-dev.apps.rm1.0a51.p1.openshiftapps.com',
+    'https://felhoalapu-lab3-felhoalapu-lab.apps.okd.fured.cloud.bme.hu'
 ]
 
 # Application definition
