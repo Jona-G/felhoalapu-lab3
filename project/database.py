31723ef57f8b5a9400bebfabb7378785a433a179
Jona-G <pribek.jonatan@gmail.com>
2026-03-14 21:21:52 +0100

    Postgres changes

--- project/database.py
+++ project/database.py
@@ -1,29 +1,20 @@
 import os
-
 from django.conf import settings
 
-
-engines = {
-    'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'django.db.backends.postgresql_psycopg2',
-    'mysql': 'django.db.backends.mysql',
-}
-
-
 def config():
-    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
-    else:
-        engine = engines['sqlite']
-    name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+    engine = os.getenv('DATABASE_ENGINE', 'sqlite').lower()
+    
+    if engine == 'postgresql':
+        return {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DATABASE_NAME', 'sampledb'),
+            'USER': os.getenv('DATABASE_USER', 'myuser'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mypassword'),
+            'HOST': os.getenv('DATABASE_HOST', 'postgresql'),
+            'PORT': os.getenv('DATABASE_PORT', '5432'),
+        }
+    
     return {
-        'ENGINE': engine,
-        'NAME': name,
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
     }
