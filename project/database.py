e26f9eb80b0c1900232e4ceb5f55d2fbceec0f39
Jona-G <pribek.jonatan@gmail.com>
2026-03-14 22:15:24 +0100

    Edit db settings

--- project/database.py
+++ project/database.py
@@ -2,19 +2,11 @@ import os
 from django.conf import settings
 
 def config():
-    engine = os.getenv('DATABASE_ENGINE', 'sqlite').lower()
-    
-    if engine == 'postgresql':
-        return {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv('DATABASE_NAME', 'sampledb'),
-            'USER': os.getenv('DATABASE_USER', 'myuser'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mypassword'),
-            'HOST': os.getenv('DATABASE_HOST', 'postgresql'),
-            'PORT': os.getenv('DATABASE_PORT', '5432'),
-        }
-    
     return {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME', 'sampledb'),
+        'USER': os.getenv('DATABASE_USER', 'myuser'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('DATABASE_HOST', 'postgresql'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
