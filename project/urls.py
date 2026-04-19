626e231ec78727596127b97c1156606853a1d9ee
Jona-G <pribek.jonatan@gmail.com>
2026-04-19 12:02:25 +0200

    Fix image upload

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
 
 from . import views
 
@@ -17,6 +18,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
